d61c532248143547240d0d06d27dc526e856b79c
tossowski <timothy_ossowski@brown.edu>
2020-01-09 13:52:39 -0500

    Added credits

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsController : MonoBehaviour
+{
+
+    Image trio;
+
+    void Start()
+    {
+        trio = GameObject.Find("Canvas").transform.Find("Panel").transform.Find("Credits").transform.Find("Spoiler").gameObject.GetComponent<Image>();
+    }
+
+    void revealSprite()
+    {
+        StartCoroutine(brighten(trio));
+    }
+
+    IEnumerator brighten(Image image)
+    {
+        while (image.color.a < 1)
+        {
+            Color prev = image.color;
+            prev.a += Time.deltaTime;
+            yield return new WaitForSeconds(0.1f);
+            image.color = prev;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Lock.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    void unlock()
+    {
+        Destroy(gameObject, 1.0f);
+    }
+}
