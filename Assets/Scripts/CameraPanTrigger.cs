0213483bc5b12f7dc2daf3ef3ba1e829a6cbb693
tossowski <timothy_ossowski@brown.edu>
2020-01-09 11:32:54 -0500

    Menacing effect

--- Assets/Scripts/CameraPanTrigger.cs
+++ Assets/Scripts/CameraPanTrigger.cs
@@ -10,10 +10,12 @@ public class CameraPanTrigger : MonoBehaviour
     protected CinemachineVirtualCamera vcam;
     private CinemachineVirtualCamera targetCam;
     private GameObject target;
+    private GameObject menacing;
 
     private float duration;
     private bool entered;
     private bool ayeayea;
+    private AudioClip JOJOLOOP;
     // Start is called before the first frame update
     public void Start()
     {
@@ -22,7 +24,9 @@ public class CameraPanTrigger : MonoBehaviour
         entered = false;
         ayeayea = false;
         vcam = GameObject.Find("VirtualCamera").GetComponent<CinemachineVirtualCamera>();
+        menacing = GameObject.Find("Canvas").transform.Find("Menacing").gameObject;
         targetCam = target.GetComponent<CinemachineVirtualCamera>();
+        JOJOLOOP = Resources.Load("Music/JojoLoop") as AudioClip;
     }
 
     // Update is called once per frame
@@ -58,8 +62,14 @@ public class CameraPanTrigger : MonoBehaviour
 
     IEnumerator playJojoMusic(float duration)
     {
-        yield return new WaitForSeconds(duration);
-        Sound.SoundFX.playSound("Music/JojoLoop", true, 0.6f);
+        yield return new WaitForSeconds(1);
+        menacing.SetActive(true);
+        yield return new WaitForSeconds(duration - 1);
+        menacing.SetActive(false);
+        GameObject sound = Resources.Load("AudioClip") as GameObject;
+        sound.GetComponent<AudioSource>().clip = JOJOLOOP;
+        sound.GetComponent<AudioSource>().volume = 0.6f;
+        var clone = Instantiate(sound, Vector3.zero, Quaternion.identity);
         GameObject.Find("Player").GetComponent<PlayerController>().resumePlayerMovement();
         targetCam.m_Priority = 5;
         GameObject.Find("GreenSkeleton").GetComponent<Animator>().SetBool("fightStarted", true);
